bc4f7459d6f44592392621df70301d7c8502638c
Tomachi (ICHIGO) <lego@aol.jp>
2020-09-28 09:42:12 +0900

    change: 調整

--- src/main/java/com/jaoafa/VCWatcher/Event/Event_VCMove.java
+++ src/main/java/com/jaoafa/VCWatcher/Event/Event_VCMove.java
@@ -1,81 +1,80 @@
 package com.jaoafa.VCWatcher.Event;
 
-import com.jaoafa.VCWatcher.Main;
 import com.jaoafa.VCWatcher.Lib.VCData;
-
+import com.jaoafa.VCWatcher.Main;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceMoveEvent;
 import net.dv8tion.jda.api.hooks.SubscribeEvent;
 
 public class Event_VCMove {
-	@SubscribeEvent
-	public void onVCMove(GuildVoiceMoveEvent event) {
-		System.out.println("onVCMove()");
-		Guild guild = event.getGuild();
-		String vcName = event.getChannelJoined().getName();
-		long vcid = event.getChannelJoined().getIdLong();
-		long userid = event.getMember().getIdLong();
-		String userstr = event.getMember().getUser().getAsTag();
+    @SubscribeEvent
+    public void onVCMove(GuildVoiceMoveEvent event) {
+        System.out.println("onVCMove()");
+        Guild guild = event.getGuild();
+        String vcName = event.getChannelJoined().getName();
+        long vcid = event.getChannelJoined().getIdLong();
+        long userid = event.getMember().getIdLong();
+        String userstr = event.getMember().getUser().getAsTag();
 
-		if (userid == 357565259151572992L) {
-			return;
-		}
+        if (userid == 357565259151572992L) {
+            return;
+        }
 
-		if (guild.getAfkChannel() != null && guild.getAfkChannel().getIdLong() == vcid) {
-			return;
-		}
+        if (guild.getAfkChannel() != null && guild.getAfkChannel().getIdLong() == vcid) {
+            return;
+        }
 
-		long channelid = Main.alertChannel(guild);
-		TextChannel channel = Main.getJDA().getTextChannelById(channelid);
-		if (channel == null) {
-			return;
-		}
+        long channelid = Main.alertChannel(guild);
+        TextChannel channel = Main.getJDA().getTextChannelById(channelid);
+        if (channel == null) {
+            return;
+        }
 
-		VCData vcdata = Main.getVCData(guild);
-		if (vcdata != null) {
-			if (vcdata.getLastVCID() == vcid || vcdata.getUserID() == userid) {
-				Main.setVCData(new VCData(guild.getIdLong(), vcid, userid));
-				return;
-			}
-		} else {
-			Main.setVCData(new VCData(guild.getIdLong(), vcid, userid));
-		}
+        VCData vcdata = Main.getVCData(guild);
+        if (vcdata != null) {
+            if (vcdata.getLastVCID() == vcid || vcdata.getUserID() == userid) {
+                Main.setVCData(new VCData(guild.getIdLong(), vcid, userid));
+                return;
+            }
+        } else {
+            Main.setVCData(new VCData(guild.getIdLong(), vcid, userid));
+        }
 
-		long count = Main.getVCMembersCountIgnoreBot(event.getChannelJoined());
-		if (count != 1) {
-			return;
-		}
+        long count = Main.getVCMembersCountIgnoreBot(event.getChannelJoined());
+        if (count != 1) {
+            return;
+        }
 
-		long lastmessageId = Main.getLastMessageId(guild);
-		if (lastmessageId != -1) {
-			long nowLastMsgID = channel.getLatestMessageIdLong();
-			if(nowLastMsgID != lastmessageId){
-				channel.retrieveMessageById(lastmessageId).queue(
-						msg -> {
-							if (msg == null) {
-								return;
-							}
-							msg.delete().queue();
-						});
-				lastmessageId = -1;
-			}
-		}
+        long lastmessageId = Main.getLastMessageId(guild);
+        if (lastmessageId != -1) {
+            long nowLastMsgID = channel.getLatestMessageIdLong();
+            if (nowLastMsgID != lastmessageId) {
+                channel.retrieveMessageById(lastmessageId).queue(
+                        msg -> {
+                            if (msg == null) {
+                                return;
+                            }
+                            msg.delete().queue();
+                        });
+                lastmessageId = -1;
+            }
+        }
 
-		if (lastmessageId != -1) {
-			channel.sendMessage(":telephone_receiver:" + vcName + "で" + userstr + "が通話をはじめました。").queue(
-					msg -> {
-						if (!Main.setLastMessageId(msg.getGuild(), msg.getIdLong())) {
-							System.out.println("setLastMessageId: failed.");
-						}
-					});
-		}else{
-			channel.retrieveMessageById(lastmessageId).queue(msg -> msg.editMessage(":telephone_receiver:" + vcName + "で" + userstr + "が通話をはじめました。").queue(
-					_msg -> {
-						if (!Main.setLastMessageId(_msg.getGuild(), _msg.getIdLong())) {
-							System.out.println("setLastMessageId: failed.");
-						}
-					}));
-		}
-	}
+        if (lastmessageId != -1) {
+            channel.sendMessage(":telephone_receiver:" + vcName + "で" + userstr + "が通話をはじめました。").queue(
+                    msg -> {
+                        if (!Main.setLastMessageId(msg.getGuild(), msg.getIdLong())) {
+                            System.out.println("setLastMessageId: failed.");
+                        }
+                    });
+        } else {
+            channel.retrieveMessageById(lastmessageId).queue(msg -> msg.editMessage(":telephone_receiver:" + vcName + "で" + userstr + "が通話をはじめました。").queue(
+                    _msg -> {
+                        if (!Main.setLastMessageId(_msg.getGuild(), _msg.getIdLong())) {
+                            System.out.println("setLastMessageId: failed.");
+                        }
+                    }));
+        }
+    }
 }
--- src/main/java/com/jaoafa/VCWatcher/Lib/VCData.java
+++ src/main/java/com/jaoafa/VCWatcher/Lib/VCData.java
@@ -1,38 +1,38 @@
 package com.jaoafa.VCWatcher.Lib;
 
 public class VCData {
-	private long guildid = -1;
-	private long lastvcid = -1;
-	private long userid = -1;
+    private long guildid;
+    private long lastvcid;
+    private long userid;
 
-	public VCData(long guildid, long lastvcid, long userid) {
-		this.guildid = guildid;
-		this.lastvcid = lastvcid;
-		this.userid = userid;
-	}
+    public VCData(long guildid, long lastvcid, long userid) {
+        this.guildid = guildid;
+        this.lastvcid = lastvcid;
+        this.userid = userid;
+    }
 
-	public long getGuildID() {
-		return guildid;
-	}
+    public long getGuildID() {
+        return guildid;
+    }
 
-	public long getLastVCID() {
-		return lastvcid;
-	}
+    public void setGuildID(long guildid) {
+        this.guildid = guildid;
+    }
 
-	public long getUserID() {
-		return userid;
-	}
+    public long getLastVCID() {
+        return lastvcid;
+    }
 
-	public void setGuildID(long guildid) {
-		this.guildid = guildid;
-	}
+    public void setLastVCID(long lastvcid) {
+        this.lastvcid = lastvcid;
+    }
 
-	public void setLastVCID(long lastvcid) {
-		this.lastvcid = lastvcid;
-	}
+    public long getUserID() {
+        return userid;
+    }
 
-	public void setUserID(long userid) {
-		this.userid = userid;
-	}
+    public void setUserID(long userid) {
+        this.userid = userid;
+    }
 
 }
